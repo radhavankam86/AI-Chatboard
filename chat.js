fbf6b0a0f750cc3a416cac99ed4a8e2aa105026a
Vankam Radha <radhavankam86@gmail.com>
2026-09-25 12:13:49 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // BACKEND URL
   // ============================================================
 
-  const BACKEND_URL = "https://your-real-render-url.onrender.com";
+ const BACKEND_URL = "https://YOUR-ACTUAL-RENDER-URL.onrender.com";
 
 
   // ============================================================
